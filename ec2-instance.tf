58f89cc8bc5fa258a37da48cff57012f5018c45a
ssr1819 <149999089+ssr1819@users.noreply.github.com>
2024-01-23 12:07:38 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"  
+}
 
+resource "aws_instance" "terraform" {
+  ami           = "ami-0c55b159cbfafe1f0"  
+  instance_type = "t2.micro"  
+
+  key_name      = "Private"
+
+  tags = {
+    Name = "terraform" 
+  }
