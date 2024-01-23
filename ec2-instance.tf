1d1e1bf0c71dba0a8006b2574401b4a4acd6084d
ssr1819 <149999089+ssr1819@users.noreply.github.com>
2024-01-23 12:09:52 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -6,8 +6,19 @@ resource "aws_instance" "terraform" {
   ami           = "ami-0c55b159cbfafe1f0"  
   instance_type = "t2.micro"  
 
-  key_name      = "Private"
+ subnet_id       = aws_subnet.my_subnet.id
+ 
+vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
+user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              service httpd start
+              chkconfig httpd on
+              firewall-cmd --zone=public --add-port=80/tcp --permanent
+              firewall-cmd --reload
+              EOF
   tags = {
     Name = "terraform" 
   }
